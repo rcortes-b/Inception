7f47051e52625e320ff3f15f928e67cfcebc62a0
rcortes- <rcortes-@student42barcelona.com>
2024-09-10 00:56:51 +0200

    update, error wp-config.php solved

--- requirements/mariadb/script.sh
+++ requirements/mariadb/script.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
 mysql_install_db
+
+service mariadb start;
+
+mysql -u $MDB_ROOT -p$MDB_ROOT_PASS -e "CREATE DATABASE $MDB_NAME; FLUSH PRIVILEGES;"
+mysql -u $MDB_ROOT -p$MDB_ROOT_PASS -e "CREATE USER '$MDB_USER'@'%' IDENTIFIED BY '$MDB_PASS'; FLUSH PRIVILEGES;"
+mysql -u $MDB_ROOT -p$MDB_ROOT_PASS -e "GRANT ALL ON $MDB_NAME.* TO '$MDB_USER'@'%' IDENTIFIED BY '$MDB_PASS' WITH GRANT OPTION; FLUSH PRIVILEGES;"
+
+mysqldadmin -u $MDB_ROOT --password=$MDB_ROOT_PASS shutdown
+
 mysqld
--- requirements/wordpress/script.sh
+++ requirements/wordpress/script.sh
@@ -3,7 +3,7 @@ cd /var/www/html
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
 ./wp-cli.phar core download --allow-root
-./wp-cli.phar config create --dbname=wordpress --dbuser=wpuser --dbpass=password --dbhost=mariadb --allow-root
-./wp-cli.phar core install --url=localhost --title=inception --admin_user=admin --admin_password=admin --admin_email=admin@admin.com --allow-root
+./wp-cli.phar config create --dbname=$MDB_NAME --dbuser=$MDB_USER --dbpass=$MDB_PASS --dbhost=$MDB_HOST --allow-root
+./wp-cli.phar core install --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_ADM_USER --admin_password=$WP_ADM_PASSWD --admin_email=$WP_ADM_EMAIL --allow-root
 
 php-fpm8.2 -F
