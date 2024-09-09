f67a4f44e1735e0b55db1fee98acfaeb608f3875
rcortes-b <raulcortes.dev@gmail.com>
2024-09-09 21:05:11 +0200

    almost ready

---
+++ requirements/wordpress/script2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd $WP_DIR
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+chmod +x wp-cli.phar
+./wp-cli.phar core download --allow-root
+./wp-cli.phar config create --dbname=$MDB_NAME --dbuser=$MDB_USER --dbpass=$MDB_PASSWD --dbhost=$MDB_HOST --allow-root
+./wp-cli.phar core install --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_ADM_USER --admin_password=$WP_ADM_PASSWD --admin_email=$WP_ADM_EMAIL --allow-root
+./wp-cli.phar user create $WP_USER $WP_USER_EMAIL --role=$WP_USER_ROLE --user_pass=$WP_USER_PASSWD --allow-root
+
+php-fpm8.2 -F
