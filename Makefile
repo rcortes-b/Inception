bf1a5d8a9744832b54eeb05c75394c80bf6e51f7
rcortes- <rcortes-@student42barcelona.com>
2024-09-10 11:23:58 +0200

    some errors solved

---
+++ Makefile
@@ -0,0 +1,16 @@
+all:
+	@docker-compose -f ./docker-compose.yml up -d --build
+
+down:
+	@docker-compose -f ./docker-compose.yml down
+
+re: clean all
+
+clean:
+	@docker stop $$(docker ps -qa);\
+	docker rm $$(docker ps -qa);\
+	docker rmi -f $$(docker images -qa);\
+	docker volume rm $$(docker volume ls -q);\
+	docker network rm inception_inception;
+
+.PHONY: all re down clean
--- requirements/mariadb/script.sh
+++ requirements/mariadb/script.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 mysql_install_db
+#mariadb-install-db
 
 service mariadb start;
 
@@ -8,6 +9,6 @@ mysql -u $MDB_ROOT -p$MDB_ROOT_PASS -e "CREATE DATABASE $MDB_NAME; FLUSH PRIVILE
 mysql -u $MDB_ROOT -p$MDB_ROOT_PASS -e "CREATE USER '$MDB_USER'@'%' IDENTIFIED BY '$MDB_PASS'; FLUSH PRIVILEGES;"
 mysql -u $MDB_ROOT -p$MDB_ROOT_PASS -e "GRANT ALL ON $MDB_NAME.* TO '$MDB_USER'@'%' IDENTIFIED BY '$MDB_PASS' WITH GRANT OPTION; FLUSH PRIVILEGES;"
 
-mysqldadmin -u $MDB_ROOT --password=$MDB_ROOT_PASS shutdown
+mysqladmin -u $MDB_ROOT --password=$MDB_ROOT_PASS shutdown
 
 mysqld
