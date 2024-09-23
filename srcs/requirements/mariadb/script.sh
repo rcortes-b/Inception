401ad6ffefca09bae940c248c73ff8d93fb4911f
rcortes- <rcortes-@student.42barcelona.com>
2024-09-23 15:59:29 +0200

    finished

--- srcs/requirements/mariadb/script.sh
+++ srcs/requirements/mariadb/script.sh
@@ -5,9 +5,19 @@ mysql_install_db
 
 service mariadb start;
 
+if [ -d "/var/lib/mysql/$MDB_NAME" ]
+
+then
+	
+	echo "Database already exists!"
+
+else
+
 mysql -u $MDB_ROOT -p$MDB_ROOT_PASS -e "CREATE DATABASE $MDB_NAME; FLUSH PRIVILEGES;"
 mysql -u $MDB_ROOT -p$MDB_ROOT_PASS -e "CREATE USER '$MDB_USER'@'%' IDENTIFIED BY '$MDB_PASS'; FLUSH PRIVILEGES;"
 mysql -u $MDB_ROOT -p$MDB_ROOT_PASS -e "GRANT ALL ON $MDB_NAME.* TO '$MDB_USER'@'%' IDENTIFIED BY '$MDB_PASS' WITH GRANT OPTION; FLUSH PRIVILEGES;"
+
+fi
 
 mysqladmin -u $MDB_ROOT --password=$MDB_ROOT_PASS shutdown
 
--- srcs/requirements/wordpress/script.sh
+++ srcs/requirements/wordpress/script.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 cd /var/www/html
+
+if [ -d ./wp-config.php ]
+
+then
+
+	echo "Wordpress is already installed!"
+
+else
+
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
 ./wp-cli.phar core download --allow-root
@@ -7,5 +16,7 @@ chmod +x wp-cli.phar
 ./wp-cli.phar core install --url="$DOMAIN_NAME" --title="$WP_TITLE" --admin_user="$WP_ADM_USER" --admin_password="$WP_ADM_PASSWD" --admin_email="$WP_ADM_EMAIL" --allow-root
 ./wp-cli.phar user create "$WP_USER" "$WP_USER_EMAIL" --role="$WP_USER_ROLE" --user_pass="$WP_USER_PASSWD" --allow-root
 ./wp-cli.phar theme activate twentytwentytwo --allow-root
+
+fi
 
 php-fpm8.2 -F
